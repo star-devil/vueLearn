c3be0e3a239e76818be7951c80b5f9d022b40b22
star-devil <707298014@qq.com>
2020-01-14 17:40:56 +0800

    日历

---
+++ 7.日历/app.vue
@@ -0,0 +1,19 @@
+<template>
+    <div> {{ msg }} </div>
+</template>
+
+<script>
+export default {
+    data(){
+        return {
+            msg: "124"
+        }
+    }
+}
+</script>
+
+<style>
+    div{
+        color: red;
+    }
+</style>
